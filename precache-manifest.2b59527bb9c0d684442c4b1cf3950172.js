193aaa01c5fcdd2581e465f6c5ae693f1009d710
Nikki <nhermann311@gmail.com>
2020-05-06 19:20:56 -0700

    Updates

---
+++ precache-manifest.2b59527bb9c0d684442c4b1cf3950172.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "06d89a78030f715cb0e3f78ca501ad3f",
+    "url": "/what_can_i_make/index.html"
+  },
+  {
+    "revision": "aa0118e8eb7fc05f0f91",
+    "url": "/what_can_i_make/static/css/main.d809cf34.chunk.css"
+  },
+  {
+    "revision": "6540315a77c182fffb15",
+    "url": "/what_can_i_make/static/js/2.7489c324.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/what_can_i_make/static/js/2.7489c324.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aa0118e8eb7fc05f0f91",
+    "url": "/what_can_i_make/static/js/main.2507904b.chunk.js"
+  },
+  {
+    "revision": "76808de6edad4c31a78c",
+    "url": "/what_can_i_make/static/js/runtime-main.29ca03e3.js"
+  }
+]);
